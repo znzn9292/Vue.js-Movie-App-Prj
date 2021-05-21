02a7e07c5b63f5fafb4f622a781209bf086247b2
jeongseonhwan <znzn9292@gmail.com>
2021-05-21 18:16:23 +0900

    Add: omdb api

---
+++ src/components/MovieList.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>Movie List ...</div>
+
+</template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="title"
+            outlined
+            @keypress.enter="searchMovies">
+        <template v-slot:prepend-inner>
+            <v-icon>search</v-icon>
+        </template>
+        <template v-slot:append >
+            <v-progress-circular
+                v-if="loading"
+                size="24"
+                color="primary"
+                indeterminate />
+
+        </template>
+
+        </v-text-field>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title: '',
+            loading: false
+        }
+    },
+    methods:  {
+        searchMovies () {
+            console.log('searchMovies')
+        }
+    }
+}
+</script>
